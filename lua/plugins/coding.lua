eed42247daebe0ab6b3625bcf2177f692fb7d6f3
dominika <dominik.albrecht@intersim.ch>
2024-06-17 14:22:22 +0200

    init

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = { ["*"] = true },
+    },
+  },
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+}
