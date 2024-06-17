a6d9ea7999ce7e4925a1f031fbcb0bb54e09763f
dominika <dominik.albrecht@intersim.ch>
2024-06-17 14:52:30 +0200

    Adjust config

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root(), reveal = true })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), reveal = true })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    {
+      "<leader>ge",
+      function()
+        require("neo-tree.command").execute({ source = "git_status", toggle = true, revael = true })
+      end,
+      desc = "Git Explorer",
+    },
+    {
+      "<leader>be",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", toggle = true, revael = true })
+      end,
+      desc = "Buffer Explorer",
+    },
+  },
+}
