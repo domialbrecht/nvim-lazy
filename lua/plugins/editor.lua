fb4b4fc17da461530ea3aa2495848564b8343f39
dominika <dominik.albrecht@intersim.ch>
2024-06-17 15:22:27 +0200

    Update filetree config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,33 +1,57 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  keys = {
-    {
-      "<leader>fe",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root(), reveal = true })
-      end,
-      desc = "Explorer NeoTree (Root Dir)",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root(), reveal = true })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), reveal = true })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      {
+        "<leader>ge",
+        function()
+          require("neo-tree.command").execute({ source = "git_status", toggle = true, revael = true })
+        end,
+        desc = "Git Explorer",
+      },
+      {
+        "<leader>be",
+        function()
+          require("neo-tree.command").execute({ source = "buffers", toggle = true, revael = true })
+        end,
+        desc = "Buffer Explorer",
+      },
     },
-    {
-      "<leader>fE",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), reveal = true })
-      end,
-      desc = "Explorer NeoTree (cwd)",
-    },
-    {
-      "<leader>ge",
-      function()
-        require("neo-tree.command").execute({ source = "git_status", toggle = true, revael = true })
-      end,
-      desc = "Git Explorer",
-    },
-    {
-      "<leader>be",
-      function()
-        require("neo-tree.command").execute({ source = "buffers", toggle = true, revael = true })
-      end,
-      desc = "Buffer Explorer",
-    },
+    opts = function(_, opts)
+      opts.window.position = "current"
+      opts.default_component_configs.file_size = {
+        enabled = false,
+      }
+      opts.default_component_configs.type = {
+        enabled = false,
+      }
+      opts.default_component_configs.last_modified = {
+        enabled = false,
+      }
+      local events = require("neo-tree.events")
+      opts.event_handlers = opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        {
+          event = events.FILE_OPENED,
+          handler = function(_)
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      })
+    end,
   },
 }
