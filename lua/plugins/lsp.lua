4a15ae381a1d56552c56b75cb59641499a929180
dominika <dominik.albrecht@intersim.ch>
2024-06-21 12:15:48 +0200

    Add twig support

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,21 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "twig",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        twiggy_language_server = {},
+      },
+    },
+  },
+}
