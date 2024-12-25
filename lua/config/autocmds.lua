41dc2e67d1871fb5e0f2295b6192e04eadc95180
dominika <dominik.albrecht@intersim.ch>
2024-12-25 18:34:46 +0100

    [feat] add epoch commands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,35 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
     end
   end,
 })
+vim.api.nvim_create_user_command("EpochToDate", function()
+  local word = vim.fn.expand("<cword>")
+  local timestamp = tonumber(word)
+  if timestamp then
+    local result = os.date("%Y-%m-%d %H:%M:%S", timestamp)
+    vim.notify("Converted: " .. result, vim.log.levels.INFO, { title = "Epoch Converter" })
+  else
+    vim.notify("Invalid Timestamp", vim.log.levels.ERROR, { title = "Epoch Converter" })
+  end
+end, {})
+
+vim.api.nvim_create_user_command("Epoch", function(opts)
+  local date_str = opts.args ~= "" and opts.args or os.date("%d/%m/%Y")
+  local pattern = "(%d+)%/(%d+)%/(%d+)"
+  local day, month, year = date_str:match(pattern)
+
+  if day and month and year then
+    local epoch = os.time({ year = year, month = month, day = day, hour = 0, min = 0, sec = 0 })
+    if epoch then
+      vim.fn.setreg("+", tostring(epoch))
+      vim.notify(
+        "Epoch timestamp: " .. epoch .. " (copied to clipboard)",
+        vim.log.levels.INFO,
+        { title = "Epoch Converter" }
+      )
+    else
+      vim.notify("Failed to convert date", vim.log.levels.ERROR, { title = "Epoch Converter" })
+    end
+  else
+    vim.notify("Invalid date format. Use DD/MM/YYYY", vim.log.levels.ERROR, { title = "Epoch Converter" })
+  end
+end, { nargs = "?" })
