81e25b196e605e1899215b223d1721a43a7d9187
dominika <dominik.albrecht@intersim.ch>
2024-12-27 10:57:31 +0100

    [feat] yazi leader fy

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,38 @@
 return {
+  ---@type LazySpec
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>fy",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        "<leader>fY",
+        function()
+          require("yazi").yazi(nil, vim.uv.cwd())
+        end,
+        desc = "Open yazi in CWD",
+      },
+      {
+        -- NOTE: this requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    ---@type YaziConfig
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = true,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+  },
   { "tpope/vim-abolish" },
   {
     "sindrets/diffview.nvim",
