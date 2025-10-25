66bbf08085e389940a87c5734d3ae323dc68fd82
dominika <dominik.albrecht@intersim.ch>
2025-10-25 17:57:23 +0200

    [feat] switch copilot chat to sidekick

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,19 +1,3 @@
-local M = {}
-
----@param kind string
-function M.pick(kind)
-  return function()
-    local actions = require("CopilotChat.actions")
-    local items = actions[kind .. "_actions"]()
-    if not items then
-      LazyVim.warn("No " .. kind .. " found on the current line")
-      return
-    end
-    local ok = pcall(require, "fzf-lua")
-    require("CopilotChat.integrations." .. (ok and "fzflua" or "telescope")).pick(items)
-  end
-end
-
 return {
   { "fei6409/log-highlight.nvim", event = "BufRead *.log", opts = {} },
   {
@@ -34,45 +18,10 @@ return {
     },
   },
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    opts = { show_help = false },
-    keys = function()
-      return {
-        { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
-        { "<leader>h", "", desc = "+ai", mode = { "n", "v" } },
-        {
-          "<leader>ha",
-          function()
-            return require("CopilotChat").toggle()
-          end,
-          desc = "Toggle (CopilotChat)",
-          mode = { "n", "v" },
-        },
-        {
-          "<leader>hx",
-          function()
-            return require("CopilotChat").reset()
-          end,
-          desc = "Clear (CopilotChat)",
-          mode = { "n", "v" },
-        },
-        {
-          "<leader>hq",
-          function()
-            local input = vim.fn.input("Quick Chat: ")
-            if input ~= "" then
-              require("CopilotChat").ask(input)
-            end
-          end,
-          desc = "Quick Chat (CopilotChat)",
-          mode = { "n", "v" },
-        },
-        -- Show help actions with telescope
-        { "<leader>hd", M.pick("help"), desc = "Diagnostic Help (CopilotChat)", mode = { "n", "v" } },
-        -- Show prompts actions with telescope
-        { "<leader>hp", M.pick("prompt"), desc = "Prompt Actions (CopilotChat)", mode = { "n", "v" } },
-      }
-    end,
+    "folke/sidekick.nvim",
+    opts = {
+      nes = { enabled = false },
+    },
   },
   {
     "neovim/nvim-lspconfig",
